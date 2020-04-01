e59c10ff167ffad9a0d2c99ad3f6ab910d356e17
Eve Noirault <eve.noirault@live.fr>
2020-04-01 16:20:10 +0100

    feat: first page and API key

--- components/Weather.js
+++ components/Weather.js
@@ -1,21 +1,51 @@
 import React from 'react';
-import { View, Text, Stylesheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Weather = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer} />
-      <View style={styles.bodyContainer} />
+    <View style={styles.weatherContainer}>
+      <View style={styles.headerContainer}>
+        <MaterialCommunityIcons size={48} name="weather-sunny" color={'#fff'} />
+        <Text style={styles.tempText}>Temperature˚</Text>
+      </View>
+      <View style={styles.bodyContainer}>
+        <Text style={styles.title}>So Sunny</Text>
+        <Text style={styles.subtitle}>It hurts my eyes!</Text>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet({
-  container: {
-    flex: 1
+const styles = StyleSheet.create({
+  weatherContainer: {
+    flex: 1,
+    backgroundColor: '#f7b733'
   },
-  headerContainer: {},
-  bodyContainer: {}
+  headerContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  tempText: {
+    fontSize: 48,
+    color: '#fff'
+  },
+  bodyContainer: {
+    flex: 2,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-end',
+    paddingLeft: 25,
+    marginBottom: 40
+  },
+  title: {
+    fontSize: 48,
+    color: '#fff'
+  },
+  subtitle: {
+    fontSize: 24,
+    color: '#fff'
+  }
 });
 
 export default Weather;
---
+++ utils/WeatherAPIKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'ab3c3aee4489c79cfeb2460767586f6c';
