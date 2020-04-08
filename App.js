11e99e32b8fa95ba469447b7d941e8c76a6e7daf
Eve Noirault <eve.noirault@live.fr>
2020-04-08 17:57:49 +0100

    feat: add a form to enter city name

--- App.js
+++ App.js
@@ -6,56 +6,120 @@ import { API_KEY } from './utils/WeatherAPIKey';
 import Weather from './components/Weather';
 
 export default class App extends React.Component {
-  state = {
-    isLoading: true,
-    temperature: 0,
-    weatherCondition: null,
-    error: null
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      temperature: 0,
+      weatherCondition: null,
+      city: null,
+      cityName: '',
+      error: null
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   };
 
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        this.fetchWeather(position.coords.latitude, position.coords.longitude);
-      },
-      error => {
-        this.setState({
-          error: 'Error Getting Weather Condtions'
-        });
-      }
-    );
-  }
+  // componentDidMount() {
+  //   navigator.geolocation.getCurrentPosition(
+  //     position => {
+  //       this.fetchWeather(position.coords.latitude, position.coords.longitude);
+  //     },
+  //     error => {
+  //       this.setState({
+  //         error: 'Error Getting Weather Condtions'
+  //       });
+  //     }
+  //   );
+  // }
 
-  fetchWeather(lat, lon) {
+  displayWeather(city) {
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=metric`
     )
       .then(res => res.json())
       .then(json => {
-        // console.log(json);
+        console.log(json)
         this.setState({
+          city: true,
+          cityName: city,
           temperature: json.main.temp,
           weatherCondition: json.weather[0].main,
           isLoading: false
-        });
-      });
+        })
+      })
+    }
+
+  // fetchWeather(lat, lon) {
+  //   fetch(
+  //     `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+  //   )
+  //     .then(res => res.json())
+  //     .then(json => {
+  //       //console.log(json);
+  //       this.setState({
+  //         temperature: json.main.temp,
+  //         weatherCondition: json.weather[0].main,
+  //         isLoading: false
+  //       });
+  //     });
+  // }
+
+  handleChange(event) {
+    this.setState({
+      city: event.target.value,
+      cityName: event.target.value
+    });
+    console.log(this.state)
+  }
+
+  handleSubmit(event) {
+    this.setState({
+      city: event.target.value,
+      cityName: event.target.value
+    });
+    console.log(this.state)
+    event.preventDefault();
+    var city = this.state.city;
+    this.displayWeather(city)
   }
 
   render() {
-    const { isLoading, weatherCondition, temperature } = this.state;
+    const { isLoading, weatherCondition, temperature, city, cityName } = this.state;
     return (
       <View style={styles.container}>
-        {isLoading ? (
-          <View style={styles.loadingContainer}>
-            <Text style={styles.loadingText}>Fetching The Weather</Text>
-          </View>
+        {city ? (
+          <div>
+            <div>
+              <form onSubmit={this.handleSubmit}>
+                <label>
+                  City:
+                  <input type="text" placeholder='Enter city' onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+              </form>
+            </div>
+            <div>
+              <Weather city={cityName} weather={weatherCondition} temperature={temperature} />
+            </div>
+          </div>
         ) : (
-          <Weather weather={weatherCondition} temperature={temperature} />
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                City:
+                <input type="text" placeholder='Enter city' onChange={this.handleChange} />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
         )}
       </View>
-    );
+
+      );
+    }
   }
-}
 
 const styles = StyleSheet.create({
   container: {
